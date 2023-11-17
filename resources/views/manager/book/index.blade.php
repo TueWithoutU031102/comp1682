555a1a36eebaaa76ed4441fb35c36a5eccc38663
Alexzvn <alex.zdea@gmail.com>
2023-11-17 21:03:26 +0700

    Refactor book index view to use async/await and update UI

--- resources/views/manager/book/index.blade.php
+++ resources/views/manager/book/index.blade.php
@@ -1,92 +1,87 @@
-<!DOCTYPE html>
-<html lang="en">
+<script src="https://cdn.jsdelivr.net/npm/moment@2.29.1/moment.min.js"></script>
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    @include('layouts.link')
-    <title>Book</title>
-</head>
+<script>
+    async function updateEvent() {
+        let url = "{{ route('manager.book.event') }}";
+        let response = await fetch(url);
+        let book = await response.json();
 
-<body>
-    <x-app-layout>
-        <x-slot name="header">
-            <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
-                {{ __('Booking') }}
-            </h2>
-        </x-slot>
-        <table class="table table-hover">
-            <thead class="thead-dark">
-                <tr>
-                    <th scope="col">Name</th>
-                    <th scope="col">Phone</th>
-                    <th scope="col">Number of people</th>
-                    <th scope="col">Arrival time</th>
-                    <th scope="col">Note</th>
-                    <th scope="col">Status</th>
-                    <th scope="col">&nbsp;</th>
-                </tr>
-            </thead>
-            <tbody id="book_data">
-                @foreach ($books as $book)
-                    <tr>
-                        <td>{{ $book->bookName }}</td>
-                        <td>{{ $book->phonenumber }}</td>
-                        <td>{{ $book->numberofPeople }}</td>
-                        <td>{{ $book->arrivalTime }}</td>
-                        <td>{{ $book->note }}</td>
-                        <td>{{ $book->status }}</td>
-                        <td>
-                            <form action="{{ route('manager.book.destroy', ['book' => $book]) }}" method="POST"
-                                class="d-inline"
-                                onsubmit="return confirm('Are you sure to delete this booking !!!???')">
-                                @csrf
-                                <button class="btn btn-danger btn-sm"><i aria-hidden="true"><i
-                                            class="fa-solid fa-trash"></i></button>
-                            </form>
-                        </td>
-                    </tr>
-                @endforeach
-            </tbody>
-        </table>
-    </x-app-layout>
-    <script src="https://cdn.jsdelivr.net/npm/moment@2.29.1/moment.min.js"></script>
+        let element = window.document.querySelector('#book_data');
+        element.innerHTML = '';
 
-    <script>
-        async function updateEvent() {
-            let url = "{{ route('manager.book.event') }}";
-            let response = await fetch(url);
-            let book = await response.json();
-
-            let element = window.document.querySelector('#book_data');
-            element.innerHTML = '';
-
-            for (const obj of book) {
-
-                let tr = `<tr>
-                    <td>${obj.bookName }</td>
-                    <td>${obj.phonenumber }</td>
-                    <td>${obj.numberofPeople }</td>
-                    <td>${moment(obj.arrivalTime).format('YYYY-MM-DD HH:mm:ss') }</td>
-                    <td>${obj.note }</td>
-                    <td>${obj.status }</td>
-                    <td>
-                        <form action="/managers/books/${obj.id}/destroy"
-                            method="POST" class="d-inline"
-                            class="d-inline" onsubmit="return confirm('Are you sure to delete this booking !!!???')">
-                            @csrf
-                            <button class="btn btn-danger btn-sm"><i aria-hidden="true"><i
-                                        class="fa-solid fa-trash"></i></button>
-                        </form>
-                    </td>
-                </tr>`
-                element.insertAdjacentHTML('beforeend', tr);
-            }
+        for (const obj of book) {
+            let tr = `<tr>
+                <td>${obj.bookName }</td>
+                <td>${obj.phonenumber }</td>
+                <td>${obj.numberofPeople }</td>
+                <td>${moment(obj.arrivalTime).format('YYYY-MM-DD HH:mm:ss') }</td>
+                <td>${obj.note}</td>
+                <td>${obj.status}</td>
+                <td>
+                    <form action="/managers/books/${obj.id}/destroy" method="POST"
+                        class="d-inline"
+                        onsubmit="confirm('Are you sure to delete this booking !!!???')">
+                        @csrf
+                        <button class="btn btn-error btn-square btn-outline btn-sm">
+                            <i class="fa-solid fa-trash"></i>
+                        </button>
+                    </form>
+                </td>
+            </tr>`
+            element.insertAdjacentHTML('beforeend', tr);
         }
+    }
 
-        setInterval(updateEvent, 1000);
-    </script>
-</body>
+    setInterval(updateEvent, 1000);
+</script>
 
-</html>
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
+            {{ __('Booking') }}
+        </h2>
+    </x-slot>
+
+    <div class="card bg-base-100 max-w-4xl mx-auto my-5">
+        <div class="card-body">
+            @include('components.notification')
+            <div class="overflow-x-auto">
+                <table class="table table-zebra">
+                    <thead class="thead-dark">
+                        <tr>
+                            <th>Name</th>
+                            <th>Phone</th>
+                            <th>Number of people</th>
+                            <th>Arrival time</th>
+                            <th>Note</th>
+                            <th>Status</th>
+                            <th>&nbsp;</th>
+                        </tr>
+                    </thead>
+                    <tbody id="book_data">
+                        @foreach ($books as $book)
+                            <tr class="hover">
+                                <td>{{ $book->bookName }}</td>
+                                <td>{{ $book->phonenumber }}</td>
+                                <td>{{ $book->numberofPeople }}</td>
+                                <td>{{ $book->arrivalTime }}</td>
+                                <td>{{ $book->note }}</td>
+                                <td>{{ $book->status }}</td>
+                                <td>
+                                    <form action="{{ route('manager.book.destroy', ['book' => $book]) }}" method="POST"
+                                        class="d-inline"
+                                        onsubmit="confirm('Are you sure to delete this booking !!!???')">
+                                        @csrf
+                                        <button class="btn btn-error btn-square btn-outline btn-sm">
+                                            <i class="fa-solid fa-trash"></i>
+                                        </button>
+                                    </form>
+                                </td>
+                            </tr>
+                        @endforeach
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </div>
+</x-app-layout>
