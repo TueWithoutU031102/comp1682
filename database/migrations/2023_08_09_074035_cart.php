0dca287b6dec832ec662943ba34d5980ab12d013
Tue <vunguyenductue@gmail.com>
2023-11-18 22:36:33 +0700

    chỉnh lại migrate trang carts

--- database/migrations/2023_08_09_074035_cart.php
+++ database/migrations/2023_08_09_074035_cart.php
@@ -16,7 +16,7 @@ return new class extends Migration {
         Schema::create('carts', function (Blueprint $table) {
             $table->id();
             $table->foreignIdFor(Menu::class)->constrained()->cascadeOnDelete();
-            $table->foreignIdFor(Session::class)->constrained()->nullOnDelete();
+            $table->foreignIdFor(Session::class)->constrained()->cascadeOnDelete();
             $table->unsignedBigInteger('quantity'); // Số lượng
             $table->string('status')->default(StatusDish::Waiting->value);
             $table->timestamps();
