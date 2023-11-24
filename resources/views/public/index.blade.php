ce3d21136677ee261bec2fd4d3b1ebe68332ba8c
Alexzvn <personal@alexzvn.me>
2023-11-25 01:54:09 +0700

    Refactor book table modal in index.blade.php

--- resources/views/public/index.blade.php
+++ resources/views/public/index.blade.php
@@ -24,19 +24,71 @@
             </div>
         </div>
     @endforeach
-    {{-- TODO: chỉnh lại cho Book table không phải iframe --}}
     <dialog id="modal" class="modal">
-        <div class="modal-box w-full h-full">
-            <article class="w-full h-full">
-                <form method="dialog">
-                    <button method="dialog" class="btn btn-sm btn-circle btn-ghost absolute right-2 top-2 ">
-                        X
-                    </button>
-                </form>
-                <iframe class="w-full h-3/4" src="{{ route('customer.book.create') }}"></iframe>
-            </article>
+        <div class="modal-box w-auto md:w-[42rem]">
+            <form method="dialog">
+                <button method="dialog" class="btn btn-sm btn-circle btn-ghost absolute right-2 top-2 ">
+                    X
+                </button>
+            </form>
+            <form class="flex flex-wrap" action="{{ route('customer.book.store') }}" method="POST">
+                @csrf
+                
+                <div class="w-full px-3 mb-6">
+                    <h1 class="text-4xl text-center mt-10 font-bold mb-2">Book table</h1>
+                    @include('components.notification')
+                    <div class="input-box">
+                        <label for="bookName"
+                            class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2">Your name:</label>
+                        <input type="text"
+                            class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+                            value="{{ old('bookName') }}" id="bookName" name="bookName">
+                    </div>
+                    <div class="input-box">
+                        <label for="phonenumber"
+                            class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2">Phone:</label>
+                        <input type="text"
+                            class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+                            value="{{ old('phonenumber') }}" id="phonenumber" name="phonenumber">
+                    </div>
+                    <div class="input-box">
+                        <label for="numberofPeople"
+                            class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2">Number of
+                            people:</label>
+                        <input type="number" id="numberofPeople"
+                            class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+                            value="{{ old('numberofPeople') }}" name="numberofPeople" min="1">
+                    </div>
+    
+                    <div class="input-box">
+                        <label for="arrivalTime"
+                            class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2">Arrival time:</label>
+                        <input type="datetime-local"
+                            class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+                            value="{{ old('arrivalTime') }}" id="arrivalTime" name="arrivalTime">
+                    </div>
+                    <div class="input-box">
+                        <label for="note"
+                            class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2">Note:</label>
+                        <input type="text"
+                            class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+                            value="{{ old('note') }}" id="note" name="note">
+                    </div>
+                    <div class="button-action">
+                        <button type="submit"
+                            class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 border border-blue-700 rounded">Submit</button>
+                    </div>
+                </div>
+            </form>
+
+          <div class="modal-action">
+            <form method="dialog">
+              <!-- if there is a button in form, it will close the modal -->
+              <button class="btn">Close</button>
+            </form>
+          </div>
         </div>
-    </dialog>
+      </dialog>
 @endsection
 
 
@@ -48,3 +100,10 @@
         }
     </script>
 @endpush
+@if ($errors->any())
+@push('scripts')
+    <script>
+        showModal();
+    </script>
+@endpush
+@endif
