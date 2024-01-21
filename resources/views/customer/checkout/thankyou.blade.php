f68a9d0ecddf7b361a37508190e660f06fc824fc
Srimped <nickey200202@gmail.com>
2024-01-22 01:05:26 +0700

    sửa theme thankyou

--- resources/views/customer/checkout/thankyou.blade.php
+++ resources/views/customer/checkout/thankyou.blade.php
@@ -5,6 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <meta name="theme-color" content="#331D8A" />
     <title>Cảm ơn</title>
 
     <link href="https://cdn.jsdelivr.net/npm/daisyui@3.9.2/dist/full.css" rel="stylesheet" type="text/css" />
