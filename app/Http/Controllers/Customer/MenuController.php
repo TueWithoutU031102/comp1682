69097ac5b4c03783fc4dbeca7b207c154f12c310
Srimped <nickey200202@gmail.com>
2024-01-21 18:03:58 +0700

    ẩn và hiện combo vào đúng giờ

--- app/Http/Controllers/Customer/MenuController.php
+++ app/Http/Controllers/Customer/MenuController.php
@@ -5,17 +5,27 @@ namespace App\Http\Controllers\Customer;
 use App\Http\Controllers\Controller;
 use App\Models\Type;
 use App\Models\Menu;
+use Carbon\Carbon;
 
 class MenuController extends Controller
 {
     //
     public function index()
     {
-        // $types = Type::all();
-        $menus = Menu::all();
-        return view("customer.menu.index"
-        // , ['types' => $types]
-        , ['menus'=> $menus]);
+        $currentTime = Carbon::now();
+        dd($menus = Menu::all());
+        if ($currentTime->isBetween(Carbon::createFromTime(18, 0), Carbon::createFromTime(18, 40))) {
+            $menus = Menu::all();
+            return view(
+                "customer.menu.index"
+                // , ['types' => $types]
+                ,
+                ['menus' => $menus]
+            );
+        } else {
+            $menus = Menu::where('type_id', '<>', 4)->get();
+            return view("customer.menu.index", ['menus' => $menus]);
+        }
     }
 
     public function show(Menu $menu)
