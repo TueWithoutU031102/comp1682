a510721b1eb2d3a8a2cb88e6b7f9200bbf9b48dd
Srimped <nickey200202@gmail.com>
2024-01-22 00:25:11 +0700

    chỉnh xóa session của người dùng

--- app/Http/Controllers/Customer/CustomerController.php
+++ app/Http/Controllers/Customer/CustomerController.php
@@ -13,11 +13,17 @@ class CustomerController extends Controller
     function __construct()
     {
         $this->middleware(function (Request $request, Closure $next) {
-            $table_id = Session::find(session()->get('customer.session'))->table->id;
+            try {
+                $table_id = Session::find(session()->get('customer.session'))->table->id;
+            } catch (\Exception $e) {
+                session()->flush();
+                return redirect()->route('index');
+            }
+
             $user_name = Session::find(session()->get('customer.session'))->name;
             $session_id = session()->get('customer.session');
             $bill = Cart::where('session_id', $session_id)->get();
-            
+
             view()->share('table_id', $table_id);
             view()->share('user_name', $user_name);
             view()->share('session_id', $session_id);
