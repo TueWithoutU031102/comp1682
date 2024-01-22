f3c2b090aa0c219178816148c8b16357052c319d
Srimped <nickey200202@gmail.com>
2024-01-23 03:24:55 +0700

    chuyển màu chữ trong checkout manager

--- resources/views/manager/checkout/index.blade.php
+++ resources/views/manager/checkout/index.blade.php
@@ -60,14 +60,19 @@
                             <td>{{ $checkout->name }}</td>
                             <td>{{ $checkout->mssv }}</td>
                             <td>{{ $checkout->phone }}</td>
-                            <!-- 
-                                Pending: text-orange-500
-                                Cash - Transfer: text-green-500
-                                Canceled: text-red-500
-                             -->
-                            <td class="text-info link" onclick="status{{ $checkout->id }}.showModal()">
+                            @if($checkout->status->name == "Pending")
+                            <td class="text-info link text-orange-500" onclick="status{{ $checkout->id }}.showModal()">
                                 {{ $checkout->status }}
                             </td>
+                            @elseif($checkout->status->name == "Cash" || $checkout->status->name == "Transfer")
+                            <td class="text-info link text-green-500" onclick="status{{ $checkout->id }}.showModal()">
+                                {{ $checkout->status }}
+                            </td>
+                            @elseif($checkout->status->name == "Canceled")
+                            <td class="text-info link text-red-500" onclick="status{{ $checkout->id }}.showModal()">
+                                {{ $checkout->status }}
+                            </td>
+                            @endif
                         </tr>
                         @endforeach
                     </tbody>
@@ -84,11 +89,23 @@
             let checkout = data.checkouts.reverse();
             let table = data.tables;
             let status = data.statuses;
+
+
+
             let element = window.document.querySelector('#checkout_data');
             element.innerHTML = '';
 
             for (const obj of checkout) {
                 let tableName = table.find(table => table.id === obj.table_id).name;
+                let statusClass = '';
+                if (obj.status === "Pending") {
+                    statusClass = 'text-orange-500';
+                } else if (obj.status === "Cash" || obj.status === "Transfer") {
+                    statusClass = 'text-green-500';
+                } else if (obj.status === "Cancel") {
+                    statusClass = 'text-red-500';
+                }
+
                 let created = document.querySelector(`#status${obj.id}`)
 
                 let dialog = created ? '' : `<dialog id="status${obj.id}" class="modal">
@@ -128,13 +145,13 @@
                         <td>${obj.name}</td>
                         <td>${obj.mssv}</td>
                         <td>${obj.phone}</td>
-                        <td class="text-info link" onclick="status${obj.id}.showModal()">
+                        <td class="text-info link ${statusClass}" onclick="status${obj.id}.showModal()">
                             ${obj.status}
                         </td>
                     </tr>`
                 element.insertAdjacentHTML('beforeend', dialog + tr);
             }
         }
-        setInterval(updateEvent, 1500);
+        setInterval(updateEvent, 1000);
     </script>
 </x-app-layout>
