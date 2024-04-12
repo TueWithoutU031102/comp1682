b6249f9f80aa23b485c0b7b21c1e0ce575ed0bd7
Tue <vunguyenductue@gmail.com>
2024-04-12 23:18:38 +0700

    set time mỗi 8h sáng thì các quantity chuyển về 10

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -2,8 +2,11 @@
 
 namespace App\Console;
 
+use App\Enums\StatusMenu;
 use Illuminate\Console\Scheduling\Schedule;
+
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
+use App\Models\Menu;
 
 class Kernel extends ConsoleKernel
 {
@@ -12,7 +15,11 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule): void
     {
+
         // $schedule->command('inspire')->hourly();
+        $schedule->call(function () {
+            Menu::where('quantity', '<=', 10)->update(['quantity' => 10, 'status' => StatusMenu::Available->value]);
+        })->dailyAt('08:00');
     }
 
     /**
@@ -20,7 +27,7 @@ class Kernel extends ConsoleKernel
      */
     protected function commands(): void
     {
-        $this->load(__DIR__.'/Commands');
+        $this->load(__DIR__ . '/Commands');
 
         require base_path('routes/console.php');
     }
